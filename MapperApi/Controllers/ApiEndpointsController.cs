5ae6070dad0d4299912a7fba4d67856887169469
Eben <eben4ammo@gmail.com>
2018-07-20 06:29:20 +0200

    Revert "remove legacy views"
    This reverts commit b9dc8a44ad7d7d4aef933810e8204ae3b5a84452.

---
+++ MapperApi/Controllers/ApiEndpointsController.cs
@@ -0,0 +1,21 @@
+﻿/***
+ * Filename: ApiEndpointsController.cs
+ * Author : ebendutoit
+ * Class   : ApiEndpointsController
+ *
+ *      Website entrypoint for api discovery
+ ***/
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mapper_Api.Controllers
+{
+    public class ApiEndpointsController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return
+                    View();
+        }
+    }
+}
