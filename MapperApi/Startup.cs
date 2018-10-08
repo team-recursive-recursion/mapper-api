d7e2c9d5af2be23b5517ea9aabbdf4d8544896d0
Eben du Toit <eben@sticitt.co.za>
2018-10-08 14:48:27 +0200

    Updated swagger file name

--- MapperApi/Startup.cs
+++ MapperApi/Startup.cs
@@ -135,7 +135,7 @@ namespace Mapper_Api
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Zoning API");
             });
             app.Use(async (context, next) =>
                 {
