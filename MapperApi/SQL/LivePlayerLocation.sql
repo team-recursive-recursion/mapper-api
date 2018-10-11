4463fbe37e51287d013bb83a94ac20ef5d938f1f
DJPretorius <20798136+DJPretorius@users.noreply.github.com>
2018-10-11 10:51:26 +0200

    Fix location query after new API was created

--- MapperApi/SQL/LivePlayerLocation.sql
+++ MapperApi/SQL/LivePlayerLocation.sql
@@ -1,15 +1,15 @@
 (SELECT l."UserID", FIRST(l."PointRaw"), MAX(l."CreatedAt") AS CreatedAt From public."LiveLocation" l Where ST_Contains(	
 	(SELECT ST_Buffer(
-					(SELECT (ST_Centroid(ST_Union(ST_GeomFromWKB(ie."PointRaw")))) FROM public."Elements" ie
-					where ie."CourseId" = '7c760b21-2cd4-4eaa-b50c-682b8228ec1f'), --Point
+					(SELECT (ST_Centroid(ST_Union(ST_GeomFromWKB(ie."Raw")))) FROM public."Elements" ie
+					where ie."ZoneID" = 'a92a2974-99db-4f73-a6c5-1f509452170b'), --Point
 
 					(SELECT max(ST_DistanceSphere(
-							ST_geomFromWKB(e."PolygonRaw"), 
-							(SELECT (ST_Centroid(ST_Union(ST_GeomFromWKB(ie."PointRaw")))) FROM public."Elements" ie
-							where ie."CourseId" = '7c760b21-2cd4-4eaa-b50c-682b8228ec1f')
+							ST_geomFromWKB(e."Raw"), 
+							(SELECT (ST_Centroid(ST_Union(ST_GeomFromWKB(ie."Raw")))) FROM public."Elements" ie
+							where ie."ZoneID" = 'a92a2974-99db-4f73-a6c5-1f509452170b')
 						))
 						FROM public."Elements" e 
-						WHERE e."CourseId" = '7c760b21-2cd4-4eaa-b50c-682b8228ec1f')/1000, -- Radius
+						WHERE e."ZoneID" = 'a92a2974-99db-4f73-a6c5-1f509452170b')/1000, -- Radius
 						'quad_segs=8' --Form
 		)),
 		ST_GeomFromWKB(l."PointRaw")
@@ -18,7 +18,7 @@
  	ORDER BY CreatedAt 
 )
 
--- '7c760b21-2cd4-4eaa-b50c-682b8228ec1f' THIS IS A COURSE ID
+-- 'a92a2974-99db-4f73-a6c5-1f509452170b' THIS IS A COURSE ID
 
 -- -- Create a function that always returns the first non-NULL item
 -- CREATE OR REPLACE FUNCTION public.first_agg ( anyelement, anyelement )
